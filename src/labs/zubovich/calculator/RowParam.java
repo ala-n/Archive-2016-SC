3161861ad70e0e280aefb6e5ae5ec06ddf023194
Ivan Poyda <i.poyda@intetics.com>
2016-05-24 18:56:08 +0300

    added novelty coefficient to enums and row params(last one shuld be checked

--- src/labs/zubovich/calculator/RowParam.java
+++ src/labs/zubovich/calculator/RowParam.java
@@ -1,6 +1,7 @@
 package labs.zubovich.calculator;
 
 import labs.zubovich.calculator.util.DevelopmentTools;
+import labs.zubovich.calculator.util.Novelty;
 import labs.zubovich.calculator.util.StandartModuleUsageEnum;
 import labs.zubovich.ui.RowEditorType;
 
@@ -16,13 +17,18 @@ public enum RowParam {
 			new RowEditorType.SelectType<>(DevelopmentTools.values())
 	),
 	STANDART_USAGES_K(
-			"КОЭФФИЦИЕНТЫ, УЧИТЫВАЮЩИЕ СТЕПЕНЬ ИСПОЛЬЗОВАНИЯ СТАНДАРТНЫХ МОДУЛЕЙ",
+			"Коэфиценты, учтывающие степень использования стандартных модулей",
 			StandartModuleUsageEnum.class,
 			new RowEditorType.SelectType<>(StandartModuleUsageEnum.values())
 	),
+	NOVELTY_COEFFICIENT(
+			"Коэфициент новизны",
+			Novelty.class,
+			new RowEditorType.SelectType<>(Novelty.values())
+	);
 
-	CoefNew("Коэфициент новизны", Double.class, new RowEditorType.NumberEditor()),
-	Test("Test", Boolean.class, new RowEditorType.TextEditor());
+//	CoefNew("Коэфициент новизны", Double.class, new RowEditorType.NumberEditor()),
+//	Test("Test", Boolean.class, new RowEditorType.TextEditor());
 
 	private String title;
 	private Class<?> type;
